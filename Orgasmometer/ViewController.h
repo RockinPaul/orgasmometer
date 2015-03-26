2ddfbc47a698403044a8082b1546739c145f558b
Pavel Zarudnev <pavel@MacBook-Air.local>
2015-03-26 18:13:45 +0300

    Initial Commit

---
+++ Orgasmometer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Orgasmometer
+//
+//  Created by Pavel on 26.03.15.
+//  Copyright (c) 2015 Zitech Mobile LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
