6aa2887d1223608a6addc63cf9525c4291f0cb2b
Pavel Zarudnev <pavel@MacBook-Air.local>
2015-03-26 16:21:15 +0300

    Initial Commit

---
+++ Orgasmometer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Orgasmometer
+//
+//  Created by Pavel on 26.03.15.
+//  Copyright (c) 2015 Zitech Mobile LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
